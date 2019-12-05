ef66bd3e945564a0884cb5b3965b07392e9c164d
git <lukechaston@gmail.com>
2019-12-05 16:42:53 +0000

    Files for front end of banking app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function useGetAccounts() {
+    // sets up local state inside function
+    // accounts = variable that stores state
+    // setAccountsData function that updates the Accounts variable
+    const [accounts, setAccountsData] = useState([])
+    useEffect(()=>{
+        // Makes request to URL:
+        fetch('http://localhost:8000/bank')
+        // formats the Json
+            .then((data)=>{return data.json()})
+            // .then (above) followed by a return passes the data down to the next .then as a parameter (below)
+            .then((data)=>{
+                // data is the array of objects in Json
+                setAccountsData(data)
+            })
+    }, accounts)
+    return accounts
+}
+function Accounts() {
+    const accounts = useGetAccounts()
+    const accountElements = accounts.map((account) =>
+        <p>{account.name}: {account.balance}</p>
+    );
+    return (
+        <div className="App">
+            <h1> Here are the accounts { accountElements }</h1>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Accounts />, document.getElementById('root'))
