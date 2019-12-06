4bd2c2b57c6e194cb521311f05455baa583c5ab5
git <lukechaston@gmail.com>
2019-12-06 14:16:56 +0000

    created submission page.

--- src/submission.js
+++ src/submission.js
@@ -1,4 +1,15 @@
 import React from "react";
-<div className="Submission">
-    <h3> Enter new amount:</h3> <input type={onsubmit} name="submission"/>
-</div>
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class submission extends React.Component {
+    render() {
+        return(
+            <div className="Submission">
+                <h3> Enter new amount:</h3> <input type={onsubmit} name="submission"/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<submission />, document.getElementById('root'));
